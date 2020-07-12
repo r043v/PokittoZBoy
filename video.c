b873f4798cd9aee651609469509eebfb41b501c3
r043v <noferov@gmail.com>
2020-07-12 10:28:01 +0200

    rewrite draw window

--- video.c
+++ video.c
@@ -11,29 +11,15 @@
 unsigned int VideoClkCounterMode, VideoClkCounterVBlank;
 uint8_t CurLY, LastLYdraw;   /* used by VideoSysUpdate */
 
-#define lcdControlRegister IoRegisters[0xFF40]
+#define lcdControlRegister (IoRegisters[0xFF40])
 
 #define pal_BGP 0xFF47
 #define pal_OBP0 0xFF48
 #define pal_OBP1 0xFF49
 
-//u_int32_t palColorTable[ 256 ] = { 0 };
-
-/*u_int8_t palColorTable[4*3] = { 0 };
-u_int8_t * pal0ColorTable = &palColorTable[4];
-u_int8_t * pal1ColorTable = &palColorTable[8];
-
-FORCE_INLINE void computePalBgColorTable(){
-  static u_int8_t last = 0;
-  register u_int8_t pal = IoRegisters[pal_BGP];
-  if( pal == last ) return;
-  palColorTable[0] = pal & 3;
-  palColorTable[1] = (pal >> 4) & 3;
-  palColorTable[2] = (pal >> 2) & 3;
-  palColorTable[3] = (pal >> 6) & 3;
-}*/
-
 /*
+u_int32_t palColorTable[ 256 ] = { 0 };
+
 void computePalColorTable( void ){
   u_int8_t i, *p = (u_int8_t*)palColorTable;
   for( i = 0; i <= 255; i++ ){
@@ -51,28 +37,23 @@ void computePalColorTable( void ){
 #define getPalette1( i ) pal1ColorTable[ i ]
 */
 
-/*FORCE_INLINE uint8_t GetGbPalette(int PalAddr, uint8_t ColIdx) {
-  switch( ColIdx ){
-    case 0:
-    return IoRegisters[PalAddr] & 3;
-
-    case 1:
-    return (IoRegisters[PalAddr] >> 4) & 3;
-
-    case 2:
-    return (IoRegisters[PalAddr] >> 2) & 3;
-
-    case 3:
-    return (IoRegisters[PalAddr] >> 6) & 3;
-  }
-
-  return 0;
-}*/
-
 uint8_t framebuffer[160*4];
 
-const u_int32_t palettes[] =
-{0x73ae0,0x4e7a0,0x25460,0x8518,0x3fff0,0x330b0,0x15650,0x4718,0x3bfa8,0x26b78,0x15438,0x7fff8,0x7db68,0x59558,0x14510,0x7fff8,0xce08,0x1d620,0x53428,0x7bc68,0xce08,0x1d620,0x53428,0x7bc68,0xce08,0x1d620,0x53428,0x7bc68,0xce08,0x1d620,0x53428,0x7bc68,0x0,0x3cc00,0x7ea60,0x7fff8,0x0,0x3cc00,0x7b0a0,0x7fff8,0x0,0x3cc00,0x7ea60,0x7fff8,0x0,0x3cc00,0x7ea60,0x7fff8,0x0,0x44e38,0x7e080,0x7fff8,0x0,0x6c000,0x7e080,0x7fff8,0x0,0x2000,0x3bf28,0x7fff8,0x0,0xf0,0x32890,0x7fff8,0x2cc08,0x41a20,0x66680,0x7f9b8,0x0,0x35428,0x52048,0x7fff8,0x0,0x3cc00,0x7ea60,0x7fff8,0x0,0x3cc00,0x7ea60,0x7fff8,0x0,0x52080,0x29560,0x7fd90,0x0,0x6c000,0x7e080,0x7ff00,0x0,0x2000,0x6e068,0x7fff8,0x0,0x14a40,0x2e2a8,0x7fff8,0x7fff8,0x7e930,0x12500,0x21830,0x0,0x6c000,0x7e080,0x7fff8,0x7ff00,0x3cc10,0x3bf28,0x7fff8,0x0,0x75b08,0x7fff8,0x7fff8,0x625c8,0x21440,0x4c980,0x4310,0x625c8,0x18c88,0x4c980,0x4310,0x0,0x10820,0xa668,0x7ebd0,0x0,0x1c6b0,0x74000,0x7fff8,0x0,0x2800,0x7c000,0x7f610,0x7df00,0x47680,0x10920,0x0,0x54ff8,0x0,0x7ff78,0x7df00,0x2bf08,0x75b08,0x7fff8,0x7fff8,0x1408,0x7cc00,0x7ff70,0x1edf0,0x0,0x7ff00,0x5d800,0x7fff8,0x5c000,0x7c000,0x7b7d0,0x7fff8,0x0,0x75b08,0x7fff8,0x7fff8,0x20e18,0x31628,0x56760,0x736a0,0x0,0x6c000,0x7e080,0x7fff8,0x0,0x2000,0x56760,0x7fff8,0x0,0x78700,0x7fc00,0x7fff8,0x10038,0x49c00,0x7e540,0x7f8b0,0x0,0x1cd0,0xe6f8,0x7fdc0,0x10038,0x2c0b0,0x514f8,0x7fff8,0x1480,0x11fb8,0x370f8,0x7fff8,0x0,0x5a248,0x73270,0x5bae8,0x0,0x6c000,0x7e080,0x7fff8,0x70718,0x110b8,0x77690,0x7fff8,0x1da00,0x63918,0x7bf98,0x7fff8};
+const u_int32_t palettes[] = {
+  0x73ae0,0x4e7a0,0x25460,0x8518,0x3fff0,0x330b0,0x15650,0x4718,0x3bfa8,0x26b78,0x15438,0x7fff8,0x7db68,0x59558,0x14510,0x7fff8,
+  0xce08,0x1d620,0x53428,0x7bc68,0xce08,0x1d620,0x53428,0x7bc68,0xce08,0x1d620,0x53428,0x7bc68,0xce08,0x1d620,0x53428,0x7bc68,
+  0x0,0x3cc00,0x7ea60,0x7fff8,0x0,0x3cc00,0x7b0a0,0x7fff8,0x0,0x3cc00,0x7ea60,0x7fff8,0x0,0x3cc00,0x7ea60,0x7fff8,
+  0x0,0x44e38,0x7e080,0x7fff8,0x0,0x6c000,0x7e080,0x7fff8,0x0,0x2000,0x3bf28,0x7fff8,0x0,0xf0,0x32890,0x7fff8,
+  0x2cc08,0x41a20,0x66680,0x7f9b8,0x0,0x35428,0x52048,0x7fff8,0x0,0x3cc00,0x7ea60,0x7fff8,0x0,0x3cc00,0x7ea60,0x7fff8,
+  0x0,0x52080,0x29560,0x7fd90,0x0,0x6c000,0x7e080,0x7ff00,0x0,0x2000,0x6e068,0x7fff8,0x0,0x14a40,0x2e2a8,0x7fff8,
+  0x7fff8,0x7e930,0x12500,0x21830,0x0,0x6c000,0x7e080,0x7fff8,0x7ff00,0x3cc10,0x3bf28,0x7fff8,0x0,0x75b08,0x7fff8,0x7fff8,
+  0x625c8,0x21440,0x4c980,0x4310,0x625c8,0x18c88,0x4c980,0x4310,0x0,0x10820,0xa668,0x7ebd0,0x0,0x1c6b0,0x74000,0x7fff8,
+  0x0,0x2800,0x7c000,0x7f610,0x7df00,0x47680,0x10920,0x0,0x54ff8,0x0,0x7ff78,0x7df00,0x2bf08,0x75b08,0x7fff8,0x7fff8,
+  0x1408,0x7cc00,0x7ff70,0x1edf0,0x0,0x7ff00,0x5d800,0x7fff8,0x5c000,0x7c000,0x7b7d0,0x7fff8,0x0,0x75b08,0x7fff8,0x7fff8,
+  0x20e18,0x31628,0x56760,0x736a0,0x0,0x6c000,0x7e080,0x7fff8,0x0,0x2000,0x56760,0x7fff8,0x0,0x78700,0x7fc00,0x7fff8,
+  0x10038,0x49c00,0x7e540,0x7f8b0,0x0,0x1cd0,0xe6f8,0x7fdc0,0x10038,0x2c0b0,0x514f8,0x7fff8,0x1480,0x11fb8,0x370f8,0x7fff8,
+  0x0,0x5a248,0x73270,0x5bae8,0x0,0x6c000,0x7e080,0x7fff8,0x70718,0x110b8,0x77690,0x7fff8,0x1da00,0x63918,0x7bf98,0x7fff8
+};
 
 u_int8_t scaling = 1;
 
@@ -100,16 +81,16 @@ u_int8_t spriteColorTable[2][4] = { { 0,0,0,0 }, { 0,0,0,0 } }, spriteCurrentPal
 uint8_t tileTempBuffer[64];
 
 /*FORCE_INLINE*/ void DrawBackground( void ){
-  unsigned int TilesDataAddr, BgTilesMapAddr, LastDisplayedTile = 0xffffffff;
+  u_int32_t TilesDataAddr, BgTilesMapAddr, LastDisplayedTile = 0xffffffff;
   uint8_t y, z ;
 
   //u_int8_t * bgColorTable = (u_int8_t*)&palColorTable[ IoRegisters[ pal_BGP ] ];
 
-  TilesDataAddr  = lcdControlRegister & 16 ? 0x8000 : 0x8800;
+  TilesDataAddr = lcdControlRegister & 16 ? 0x8000 : 0x8800;
 
-  y = CurLY + IoRegisters[0xFF42]; // scanline + scroll y
+  y = CurLY + IoRegisters[ 0xFF42 ]; // scanline + scroll y
   z = ( y & 7 ) << 1 ; // tile offset ( 2B tile )
-  u_int8_t * tileBfPtrOffset = tileTempBuffer + ( z << 2 ) ; // out tile offset ( 2B -> 8B )
+  u_int8_t * tileBfPtrOffset = tileTempBuffer + ( z << 2 ) ; // out tile offset ( 2B -> 8B tile )
 
   u_int8_t TileNum = IoRegisters[0xFF43] >> 3;  /* 0xFF43 is the SCX register */
 
@@ -125,7 +106,7 @@ uint8_t tileTempBuffer[64];
 
     if( ++tilePtr == lastTile ) tilePtr -= 32;
 
-    u_int8_t * tileBfPtr = tileBfPtrOffset ;
+    register u_int8_t * tileBfPtr = tileBfPtrOffset ;
 
     if( TileToDisplay != LastDisplayedTile ){
       LastDisplayedTile = TileToDisplay;
@@ -133,7 +114,7 @@ uint8_t tileTempBuffer[64];
       u_int8_t * tileDataTempPtr = tileDataPtr + ( TileToDisplay << 4 );
 
       register u_int32_t
-        b1 = (*tileDataTempPtr++)<<1,
+        b1 = (*tileDataTempPtr++) << 1,
         b2 = *tileDataTempPtr ;
 
       *tileBfPtr++ = bgColorTable[
@@ -198,112 +179,95 @@ uint8_t tileTempBuffer[64];
 }
 
 /*FORCE_INLINE*/ void DrawWindow(void){
-  /*
-   Some infos about the window...
-
-   WindowPosX = 7    [FF4Bh]  (should be >= 0 and <= 166)
-   WindowPosY = 0    [FF4Ah]  (should be >= 0 and <= 143)
-
-   Window tiles can be located at 8000h-8FFFh or 8800h-97FFh (just like for the background)
-   At 8000h-8FFFh tiles are numbered from 0 to 255, while at 8800h-97FFh tiles are ranging from -128 to 127.
-
-   The LCDC register is at FF40h.
-   Bit 6 - Window tile map address (0: 9800h-9BFFh, 1: 9C00h-9FFFh)
-   Bit 5 - Window Display (0: off, 1: on)
-   Bit 4 - Window & background tile data address (0: 8800h-97FFh, 1: 8000h-8FFFh)
-   Bit 0 - Window & background display (0: off, 1: on)
-
-   The palette for both background and window is located at FF47h (BGP)
-  */
-
-  if( CurLY > 143
-  || IoRegisters[0xFF4B] > 166
-  || IoRegisters[0xFF4A] > CurLY
-  || IoRegisters[0xFF4B] < 7
+  if( CurLY > 143 // out of screen bottom
+  || IoRegisters[0xFF4B] > 166 // out of screen right
+  || IoRegisters[0xFF4B] < 7 // out of screen left
+  || IoRegisters[0xFF4A] > CurLY // show after current scanline
   ) return;
 
-/*  if( bgCurrentPal != IoRegisters[pal_BGP] ){
-    bgCurrentPal = IoRegisters[pal_BGP];
-    bgColorTable[0] = bgCurrentPal & 3;
-    bgColorTable[1] = (bgCurrentPal >> 4) & 3;
-    bgColorTable[2] = (bgCurrentPal >> 2) & 3;
-    bgColorTable[3] = (bgCurrentPal >> 6) & 3;
-  }
-*/
 //  u_int8_t * bgColorTable = (u_int8_t*)&palColorTable[ IoRegisters[ pal_BGP ] ];
 
-  static signed int x, y, z, t, UbyteBuff1, UbyteBuff2, pixrow;
-  static signed int TilesMapAddr, TilesDataAddr, TileNum, TileToDisplay, PixelX, TileTempAddr;
-//  static uint8_t TileBufferWin[64];
   u_int32_t LastDisplayedTile = 0xffffffff;
-  u_int8_t * TileBufferWin = tileTempBuffer;
+  u_int32_t TilesDataAddr = lcdControlRegister & 16 ? 0x8000 : 0x8800 ;
 
-    TilesDataAddr  = lcdControlRegister & 16 ? 0x8000 : 0x8800;
-    TilesMapAddr   = lcdControlRegister & 64 ? 0x9C00 : 0x9800;
+  u_int8_t pixrow = CurLY - IoRegisters[ 0xFF4A ] ; // window line to draw
+  u_int8_t TileNum = ( pixrow >> 3 ) << 5 ; /* tile data index */
+  u_int8_t z = ( pixrow & bx00000111 ) << 1; // offset y in gb tile (2B)
 
-    pixrow = (CurLY - IoRegisters[0xFF4A]);
-    y = (pixrow >> 3);  /* map offset y */
-    TileNum = (y << 5);     /* tile data index */
-    pixrow &= bx00000111;  /* tile offset y */
+  u_int32_t TilesMapAddr = ( lcdControlRegister & 64 ? 0x9C00 : 0x9800 ) ;
 
-//    register u_int8_t * frameBfPtr = framebuffer ;
-//    u_int8_t *tilePtr = &VideoRAM[ TilesMapAddr + TileNum ], *lastTile = &tilePtr[ 32 - TileNum ];
-//    u_int8_t *tileDataPtr = &VideoRAM[ TilesDataAddr ];
+  u_int8_t * tileBfPtrOffset = tileTempBuffer + ( z << 2 ) ; // out tile offset ( 2B -> 8B tile )
 
-//    while(1){
-        for (x = 0; x < 32; x++) {
-//      TileToDisplay = TilesDataAddr == 0x8000 ? *tilePtr : UbyteToByte( *tilePtr ) + 128 ;
+  register u_int8_t * frameBfPtr = &framebuffer[ IoRegisters[ 0xFF4B ] - 7 ];
+  u_int8_t *tilePtr = &VideoRAM[ TilesMapAddr + TileNum ] ;
+  u_int8_t *tileDataPtr = &VideoRAM[ TilesDataAddr + z ];
 
-          if (TilesDataAddr == 0x8000) {
-              TileToDisplay = VideoRAM[TilesMapAddr + TileNum];
-            } else {
-              TileToDisplay = UbyteToByte(VideoRAM[TilesMapAddr + TileNum]) + 128;
-          }
+  while(1){
+    u_int8_t TileToDisplay = TilesDataAddr == 0x8000 ? *tilePtr : UbyteToByte( *tilePtr ) + 128 ;
+    tilePtr += 1;
 
-    if( TileToDisplay != LastDisplayedTile ){ /* Compute the tile only if it's different than the previous one (otherwise just */
-      LastDisplayedTile = TileToDisplay;      /* reuse the same data) - I named this a "micro tile cache" in the changelog. */
+    register u_int8_t * tileBfPtr = tileBfPtrOffset ;
 
+    if( TileToDisplay != LastDisplayedTile ){
+      LastDisplayedTile = TileToDisplay;
 
-          z = (pixrow << 1);   /*      Get only the tile's y line related to current scanline */
-          TileTempAddr = TilesDataAddr + (TileToDisplay << 4) + z;  /* << 4 is the same than *16 (just much faster) */
-          z <<= 2;
-          /*FOR z = 0 TO 15 STEP 2 */
-            UbyteBuff1 = VideoRAM[TileTempAddr];     /* << 4 is the same than *16 (just much faster) */
-            UbyteBuff2 = VideoRAM[TileTempAddr + 1]; /* << 4 is the same than *16 (just much faster) */
-            TileBufferWin[z + 7] = bgColorTable[// GetGbPalette(pal_BGP,
-						 (UbyteBuff2&1) | ((UbyteBuff1 & 1) << 1)];//);      /* Note: I am using "SHL 2" here instead of "*4" (faster!) */
-            TileBufferWin[z + 6] = bgColorTable[//GetGbPalette(pal_BGP,
-						((UbyteBuff2>>1)&1) | (((UbyteBuff1>>1)&1) << 1)];//);
-	    TileBufferWin[z + 5] = bgColorTable[//GetGbPalette(pal_BGP,
-						((UbyteBuff2>>2)&1) | (((UbyteBuff1>>2)&1) << 1)];//);
-	    TileBufferWin[z + 4] = bgColorTable[//GetGbPalette(pal_BGP,
-						((UbyteBuff2>>3)&1) | (((UbyteBuff1>>3)&1) << 1)];//);
-	    TileBufferWin[z + 3] = bgColorTable[//GetGbPalette(pal_BGP,
-						((UbyteBuff2>>4)&1) | (((UbyteBuff1>>4)&1) << 1)];//);
-	    TileBufferWin[z + 2] = bgColorTable[//GetGbPalette(pal_BGP,
-						((UbyteBuff2>>5)&1) | (((UbyteBuff1>>5)&1) << 1)];//);
-	    TileBufferWin[z + 1] = bgColorTable[//GetGbPalette(pal_BGP,
-						((UbyteBuff2>>6)&1) | (((UbyteBuff1>>6)&1) << 1)];//);
-	    TileBufferWin[z] =     bgColorTable[//GetGbPalette(pal_BGP,
-						((UbyteBuff2>>7)&1) | (((UbyteBuff1>>7)&1) << 1)];//);
-          /*NEXT z */
+      u_int8_t * tileDataTempPtr = tileDataPtr + ( TileToDisplay << 4 );
 
-      } // cache
-          PixelX = ((x << 3) + IoRegisters[0xFF4B] - 7);  /* 0xFF4B is the WX register (starts at 7!)   / (x << 3) is the same than (x * 8), but faster */
-          z = (((CurLY - IoRegisters[0xFF4A]) & 7) << 3);   /* "AND bx0111" is used instead of "MOD 8" and "SHL 3" instead of "* 8" */
-          for (t = 0; t < 8; t++) {
-            if ((PixelX >= 0) && (PixelX < 160)) {
-              /*ScreenBuffer(PixelX, CurScanline) = GbPalette(TileBufferWin(z), 0) */
-              setPixel(PixelX,0, TileBufferWin[z] + 12 ); // | 32; /* | 32 is for marking it as 'window' (for possible colorization) */
-            }
-            PixelX++;  /* add 1 instead of relying on t value (faster) */
-            z++;
-          }
-          TileNum++;   /* I'm adding 1 instead of relying on x value (faster) */
-        }
-/*      }
+      register u_int32_t b1 = ( *tileDataTempPtr++ ) << 1 ;
+      register u_int32_t b2 = *tileDataTempPtr ;
+
+      *tileBfPtr++ = bgColorTable[
+      ( ( b1 & bx100000000 )
+      | ( b2 &  bx10000000 ) ) >> 7
+      ];
+
+      *tileBfPtr++ = bgColorTable[
+      ( ( b1 & bx10000000 )
+      | ( b2 & bx01000000 ) ) >> 6
+      ];
+
+      *tileBfPtr++ = bgColorTable[
+      ( ( b1 & bx01000000 )
+      | ( b2 & bx00100000 ) ) >> 5
+      ];
+
+      *tileBfPtr++ = bgColorTable[
+      ( ( b1 & bx00100000 )
+      | ( b2 & bx00010000 ) ) >> 4
+      ];
+
+      *tileBfPtr++ = bgColorTable[
+      ( ( b1 & bx00010000 )
+      | ( b2 & bx00001000 ) ) >> 3
+      ];
+
+      *tileBfPtr++ = bgColorTable[
+      ( ( b1 & bx00001000 )
+      | ( b2 & bx00000100 ) ) >> 2
+      ];
+
+      *tileBfPtr++ = bgColorTable[
+      ( ( b1 & bx00000100 )
+      | ( b2 & bx00000010 ) ) >> 1
+      ];
+
+      *tileBfPtr = bgColorTable[
+        ( b1 & bx00000010 )
+      | ( b2 & bx00000001 )
+      ];
+
+      tileBfPtr -= 7;
     }
-  }*/
+
+    // crop right
+    if( frameBfPtr > &framebuffer[ 151 ] ){
+      while( frameBfPtr != &framebuffer[160] ) *frameBfPtr++ = *tileBfPtr++;
+      return;
+    }
+
+    register u_int8_t * frameBfPtrEnd = &frameBfPtr[ 8 ] ;
+    while( frameBfPtr != frameBfPtrEnd ) *frameBfPtr++ = *tileBfPtr++;
+  }
 }
 
 #define u32 u_int32_t
